1928a97e6ad677e001333f31f07b9ce86a3520f3
Hamid <hamidprime@gmail.com>
2022-04-02 03:36:10 +0430

    Add settings UI (in progress).

--- main.js
+++ main.js
@@ -7,44 +7,33 @@ const hud = {
     creatures: document.getElementById("hud-creatures")
 }
 
-const world = new World(canvas, createObstacles(), hud);
-const creatures = createCreatures();
-world.maleCreatures = creatures.males;
-world.femaleCreatures = creatures.females;
+const defaultObstacles = [{x: 0, y: 400, w: 500, h: 50}, {x: 900, y: 0, w: 50, h: canvas.height - 120}];
+let obstacles = defaultObstacles;
+let foodInterval = null;
+let worldStatus = 'stop';
+let animationRequestID = null;
+let world = null;
 
 function animation() {
-    requestAnimationFrame(animation);
+    animationRequestID = requestAnimationFrame(animation);
     world.draw();
 }
 
-setTimeout(() => {
-    if (world.maleCreatures.length + world.femaleCreatures.length === 0) return;
-    for (let i = 0; i < world.mutationDensity; i++) {
-        const creature = Math.random() > 0.5 ? world.maleCreatures[Math.floor(Math.random() * world.maleCreatures.length)] :
-            world.femaleCreatures[Math.floor(Math.random() * world.femaleCreatures.length)];
-        if (!creature) return;
-        if (Math.random() > 0.5) {
-            creature.eye = true;
-        } else {
-            creature.flagella = true;
+function startFoodGenerationInterval() {
+    foodInterval = setInterval(() => {
+        if (world.getWorldFoodsCount() >= 3000)
+            return;
+        for (let i = 0; i < world.foodDensity; i++) {
+            let x = Math.floor((Math.random() * (canvas.width - 40 + 1)) + 10);
+            let y = Math.floor((Math.random() * (canvas.height - 40 + 1)) + 10);
+            const food = new Food(x, y);
+            if (!food.collideToObstacles(world.obstacles))
+                world.putFoodInTheFoodMatrix(food);
         }
-    }
-}, world.mutationInterval);
-setInterval(() => {
-    if (world.getWorldFoodsCount() >= 3000)
-        return;
-    for (let i = 0; i < world.foodDensity; i++) {
-        let x = Math.floor((Math.random() * (canvas.width - 40 + 1)) + 10);
-        let y = Math.floor((Math.random() * (canvas.height - 40 + 1)) + 10);
-        const food = new Food(x, y);
-        if (!food.collideToObstacles(world.obstacles))
-            world.putFoodInTheFoodMatrix(food);
-    }
-}, world.foodGenerationInterval);
-
-function createObstacles() {
-    return [{x: 0, y: 400, w: 500, h: 50},
-        {x: 900, y: 0, w: 50, h: canvas.height - 120}]
+    }, world.foodGenerationInterval);
+}
+function stopFoodGenerationInterval() {
+    if(foodInterval) clearInterval(foodInterval);
 }
 
 function createCreatures() {
@@ -61,7 +50,6 @@ function createCreatures() {
     }
     return {males, females};
 }
-
 function createCreature() {
     let x = Math.floor(Math.random() * ((canvas.width - world.creatureSize) - 10 + 1) + 10);
     let y = Math.floor(Math.random() * ((canvas.height - world.creatureSize) - 10 + 1) + 10);
@@ -69,4 +57,39 @@ function createCreature() {
     return creature.collideToObstacles() ? null : creature;
 }
 
-animation();
+function stop() {
+    worldStatus = 'stop';
+
+    stopFoodGenerationInterval();
+
+    const c = canvas.getContext('2d');
+    c.fillStyle = 'rgb(20, 20, 20)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    cancelAnimationFrame(animationRequestID);
+}
+
+function start() {
+    if(worldStatus === 'stop') {
+        worldStatus = 'play';
+        world = new World(canvas, obstacles, hud);
+        const creatures = createCreatures()
+        world.maleCreatures = creatures.males;
+        world.femaleCreatures = creatures.females;
+
+        startFoodGenerationInterval();
+        animation();
+    } else if(worldStatus === 'pause') {
+        worldStatus = 'play';
+        startFoodGenerationInterval();
+        animation();
+    }
+}
+
+function pause() {
+    worldStatus = 'pause';
+    stopFoodGenerationInterval();
+    cancelAnimationFrame(animationRequestID);
+}
+
+start();
